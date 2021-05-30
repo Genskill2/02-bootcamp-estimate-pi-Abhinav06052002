76a7649831d812db98acbcf3cf9216a0eb68a1d1
Abhinav <jammisaiabhinav@gmail.com>
2021-05-30 17:05:40 +0530

    Adds wallis and monte_carlo functions

--- estimate.py
+++ estimate.py
@@ -1,7 +1,25 @@
 import math
 import unittest
+from random import random
+
+def wallis(n):
+    p=1
+    for c in range(1,n+1):
+        p=p*((4*c*c)/(4*c*c-1))
+       
+    return 2*p 
+    
+def monte_carlo(n):
+    inside=0
+    for c in range(1,n+1):
+        x=random()
+        y=random()
+        if x*x+y*y<=1:
+            inside=inside+1
+    return 4*(inside/n)    
 
 class TestWallis(unittest.TestCase):
+        
     def test_low_iters(self):
         for i in range(0, 5):
             pi = wallis(i)
@@ -11,9 +29,10 @@ class TestWallis(unittest.TestCase):
         for i in range(500, 600):
             pi = wallis(i)
             self.assertTrue(abs(pi - math.pi) < 0.01, msg=f"Estimate with even {i} iterations is {pi} which is not accurate enough.\n")
-
+       
 
 class TestMC(unittest.TestCase):
+           
     def test_randomness(self):
         pi0 = monte_carlo(15000)
         pi1 = monte_carlo(15000)
@@ -26,7 +45,7 @@ class TestMC(unittest.TestCase):
         for i in range(500, 600):
             pi = monte_carlo(i)
             self.assertTrue(abs(pi - math.pi) < 0.4, msg=f"Estimate with even {i} iterations is {pi} which is not accurate enough.\n")
-        
+                               
     
 if __name__ == "__main__":
     unittest.main()
